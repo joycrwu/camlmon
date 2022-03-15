53618bf5117071243a171153c8ec2dc587f4bbb6
Joyce Wu <jyw55@cornell.edu>
2022-03-15 17:22:19 -0400

    changed command

--- src/command.ml
+++ src/command.ml
@@ -1,12 +1,12 @@
 type command =
   | Run
-  | Attack of string
+  | Attack of int
 
 exception Invalid_input
 
 let input (bat : Battle.t) =
   let k = Graphics.read_key () in
   match k with
-  | '1' -> Attack "test"
+  | '1' -> Attack 1
   | 'r' -> Run
   | _ -> raise Invalid_input
