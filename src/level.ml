bbdaac86ef2c62f662552ddde2b73c77c6aca59b
Yuni Park <yp247@cornell.edu>
2022-03-30 00:14:14 -0400

    commiting affinity changes

--- src/level.ml
+++ src/level.ml
@@ -18,9 +18,11 @@ type t = {
   start_tile : int * int;
   characterid : string;
   enemylist : string list;
+  affinity_order : string list;
 }
 
 let currentlvl_character (lvl : t) = lvl.characterid
 let get_characterid (lvl : t) = lvl.characterid
 let start_location (lvl : t) = lvl.start_tile
 let get_map (lvl : t) = lvl.level_id
+let get_affinity_order (lvl : t) = lvl.affinity_order
