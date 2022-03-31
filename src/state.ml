3ce6072732f661268d2034d9843251095ece7129
Ruby Wang <rw433@cornell.edu>
2022-03-30 20:42:49 -0400

    a bunch of states, may not be useful

--- src/state.ml
+++ src/state.ml
@@ -17,10 +17,12 @@ type t = {
   character : Character.t;
   map_id : string;
   health : int;
-  inbattle : bool;
+  levelmap : string;
   location : int * int;
   alive : bool;
   fought : string list;
+  usable : string list;
+  inventory : string list;
 }
 
 let init_state (lvl : Level.t) character =
@@ -29,10 +31,12 @@ let init_state (lvl : Level.t) character =
       character;
       map_id = get_map lvl;
       health = 0;
-      inbattle = false;
+      levelmap = "level";
       location = start_location lvl;
       alive = true;
       fought = [];
+      usable = [];
+      inventory = [];
     }
   in
   t
@@ -43,10 +47,12 @@ let fought (st : t) ene =
       character = st.character;
       map_id = st.map_id;
       health = st.health;
-      inbattle = false;
+      levelmap = "level";
       location = st.location;
       alive = true;
       fought = ene :: st.fought;
+      usable = [];
+      inventory = [];
     }
   in
   t
@@ -59,8 +65,70 @@ let move st x y =
     character = st.character;
     map_id = st.map_id;
     health = st.health;
-    inbattle = st.inbattle;
+    levelmap = st.levelmap;
     location = (x, y);
     alive = st.alive;
     fought = st.fought;
+    usable = [];
+    inventory = [];
+  }
+
+(*not done, probably needs a way to decide which battle it is
+  entering *)
+let battle st x y =
+  {
+    character = st.character;
+    map_id = st.map_id;
+    health = st.health;
+    levelmap = "fight";
+    location = (x, y);
+    alive = st.alive;
+    fought = st.fought;
+    usable = [];
+    inventory = [];
+  }
+
+(* hatchery is a new location, where u can get new characters by
+   gatchaing for them. Not yet implemented *)
+let hatchary st x y =
+  {
+    character = st.character;
+    map_id = st.map_id;
+    health = st.health;
+    levelmap = "hatchary";
+    location = (x, y);
+    alive = st.alive;
+    fought = st.fought;
+    usable = st.usable;
+    inventory = st.inventory;
+  }
+
+(* obtainchar is for when u obtain a new character from the hatchery.
+   Not yet implemented!!! *)
+let obtainchar st chara =
+  {
+    character = st.character;
+    map_id = st.map_id;
+    health = st.health;
+    levelmap = st.levelmap;
+    location = st.location;
+    alive = st.alive;
+    fought = st.fought;
+    usable = chara :: st.usable;
+    inventory = [];
+  }
+
+(* getitem is for when u obtain a new character from the hatchery. Not
+   yet implemented!!! *)
+let getitem st item =
+  {
+    character = st.character;
+    map_id = st.map_id;
+    health = st.health;
+    levelmap = st.levelmap;
+    location = st.location;
+    alive = st.alive;
+    fought = st.fought;
+    usable = st.usable;
+    inventory = item :: st.inventory;
   }
