895ae496ab4394a52c861a1571731cd4199ae620
Ruby Wang <rw433@cornell.edu>
2022-04-04 00:40:58 -0400

    dance dance revolution no more

---
+++ assets/citations.md
@@ -0,0 +1,3 @@
+ground.png and forest.png are created by skatefilter5
+https://www.deviantart.com/skatefilter5/art/Pokemon-Ground-600573792
+https://www.deviantart.com/skatefilter5/art/Pokemon-Safari-Plants-MV-628605621
--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,4 @@
-open Graphics
+(* open Graphics *)
 open Game
 open Raylib
 
@@ -6,74 +6,45 @@ open Raylib
 
 let _ = Random.self_init ()
 
-let flush_kp () =
-  while key_pressed () do
-    let _ = read_key () in
-    ()
-  done
+(* let flush_kp () = while key_pressed () do let _ = read_key () in ()
+   done *)
 
 (** this code was copied from stackoverflow
     https://stackoverflow.com/questions/36263152/simple-ocaml-graphics-progam-that-close-before-its-window-is-displayed*)
 
-let rec interactive () =
-  let event = wait_next_event [ Key_pressed ] in
-  if event.key == 'p' then exit 0 else print_char event.key;
-  Graphics.clear_graph ();
-  Graphics.auto_synchronize true;
-  interactive ()
+(* let rec interactive () = let event = wait_next_event [ Key_pressed ]
+   in if event.key == 'p' then exit 0 else print_char event.key;
+   Graphics.clear_graph (); Graphics.auto_synchronize true; interactive
+   () *)
 
 (** https://stackoverflow.com/questions/6390631/ocaml-module-graphics-queuing-keypresses *)
-let bottom_bar () =
-  Graphics.open_graph "";
-  Graphics.set_color (rgb 230 251 255);
-  Graphics.fill_rect 0 90 600 490;
-  Graphics.set_color (rgb 0 153 230);
-  Graphics.fill_rect 20 420 560 10;
-  Graphics.set_color (rgb 25 178 255);
-  Graphics.fill_rect 20 400 560 10;
-  Graphics.set_color (rgb 77 195 255);
-  Graphics.fill_rect 20 380 560 10;
-  Graphics.set_color (rgb 128 212 255);
-  Graphics.fill_rect 20 360 560 10;
-  Graphics.set_color (rgb 153 221 255);
-  Graphics.fill_rect 20 340 560 10;
-  Graphics.set_color (rgb 153 221 255);
-  Graphics.fill_rect 20 320 560 10;
-  Graphics.set_color (rgb 179 229 255);
-  Graphics.fill_rect 20 300 560 10;
-  Graphics.set_color (rgb 204 238 255);
-  Graphics.fill_rect 20 280 560 10;
-  Graphics.set_color (rgb 204 238 255);
-  Graphics.fill_rect 20 260 560 10;
-  Graphics.set_color (rgb 204 238 255);
-  Graphics.fill_rect 20 240 560 10;
-  Graphics.set_color (rgb 204 238 255);
-  Graphics.fill_rect 20 220 560 10;
-  Graphics.set_color (rgb 204 238 255);
-  Graphics.fill_rect 20 200 560 10;
-  Graphics.set_color (rgb 204 238 255);
-  Graphics.fill_rect 20 180 560 10;
-  Graphics.set_color (rgb 204 238 255);
-  Graphics.fill_rect 20 160 560 10;
-  Graphics.set_color (rgb 204 238 255);
-  Graphics.fill_rect 20 140 560 10;
-  Graphics.set_color (rgb 230 247 255);
-  Graphics.fill_rect 20 120 560 10;
-  Graphics.set_color (rgb 230 247 255);
-  Graphics.fill_rect 20 100 560 10;
-  Graphics.set_color (rgb 255 186 82);
-  Graphics.fill_ellipse 150 95 140 60;
-  Graphics.set_color (rgb 255 186 82);
-  Graphics.fill_ellipse 460 270 140 60;
-
-  Graphics.set_color (rgb 128 170 255);
-  Graphics.fill_rect 0 0 600 90;
-  Graphics.set_color (rgb 179 242 255);
-  Graphics.fill_rect 10 5 280 80;
-  Graphics.set_color (rgb 179 242 255);
-  Graphics.fill_rect 310 5 280 80;
-
-  Graphics.set_color (rgb 0 0 0)
+(* let bottom_bar () = Graphics.open_graph ""; Graphics.set_color (rgb
+   230 251 255); Graphics.fill_rect 0 90 600 490; Graphics.set_color
+   (rgb 0 153 230); Graphics.fill_rect 20 420 560 10; Graphics.set_color
+   (rgb 25 178 255); Graphics.fill_rect 20 400 560 10;
+   Graphics.set_color (rgb 77 195 255); Graphics.fill_rect 20 380 560
+   10; Graphics.set_color (rgb 128 212 255); Graphics.fill_rect 20 360
+   560 10; Graphics.set_color (rgb 153 221 255); Graphics.fill_rect 20
+   340 560 10; Graphics.set_color (rgb 153 221 255); Graphics.fill_rect
+   20 320 560 10; Graphics.set_color (rgb 179 229 255);
+   Graphics.fill_rect 20 300 560 10; Graphics.set_color (rgb 204 238
+   255); Graphics.fill_rect 20 280 560 10; Graphics.set_color (rgb 204
+   238 255); Graphics.fill_rect 20 260 560 10; Graphics.set_color (rgb
+   204 238 255); Graphics.fill_rect 20 240 560 10; Graphics.set_color
+   (rgb 204 238 255); Graphics.fill_rect 20 220 560 10;
+   Graphics.set_color (rgb 204 238 255); Graphics.fill_rect 20 200 560
+   10; Graphics.set_color (rgb 204 238 255); Graphics.fill_rect 20 180
+   560 10; Graphics.set_color (rgb 204 238 255); Graphics.fill_rect 20
+   160 560 10; Graphics.set_color (rgb 204 238 255); Graphics.fill_rect
+   20 140 560 10; Graphics.set_color (rgb 230 247 255);
+   Graphics.fill_rect 20 120 560 10; Graphics.set_color (rgb 230 247
+   255); Graphics.fill_rect 20 100 560 10; Graphics.set_color (rgb 255
+   186 82); Graphics.fill_ellipse 150 95 140 60; Graphics.set_color (rgb
+   255 186 82); Graphics.fill_ellipse 460 270 140 60; Graphics.set_color
+   (rgb 128 170 255); Graphics.fill_rect 0 0 600 90; Graphics.set_color
+   (rgb 179 242 255); Graphics.fill_rect 10 5 280 80; Graphics.set_color
+   (rgb 179 242 255); Graphics.fill_rect 310 5 280 80;
+   Graphics.set_color (rgb 0 0 0) *)
 
 let health_bar_ally bat () =
   (* Graphics.open_graph ""; *)
@@ -82,12 +53,9 @@ let health_bar_ally bat () =
   let character_currhp = float_of_int (Battle.character_hp bat) in
   let percent_health = character_currhp /. character_maxhp in
   let num_health = int_of_float (percent_health *. 100.) in
-  if num_health > Character.get_hp character then (
-    Graphics.set_color (rgb 0 0 0);
-    Graphics.fill_rect 50 115 100 10)
-  else (
-    Graphics.set_color (rgb 0 0 0);
-    Graphics.fill_rect 50 115 num_health 10)
+  if num_health > Character.get_hp character then
+    Raylib.draw_rectangle 50 115 100 10 Color.black
+  else Raylib.draw_rectangle 50 115 num_health 10 Color.black
 
 let health_bar_enemy bat () =
   (* Graphics.open_graph ""; *)
@@ -96,12 +64,14 @@ let health_bar_enemy bat () =
   let enemy_currhp = float_of_int (Battle.enemy_hp bat) in
   let percent_health = enemy_currhp /. enemy_maxhp in
   let num_health = int_of_float (percent_health *. 100.) in
-  if num_health > Character.get_hp enemy then (
-    Graphics.set_color (rgb 0 0 0);
-    Graphics.fill_rect 450 390 100 10)
-  else (
-    Graphics.set_color (rgb 0 0 0);
-    Graphics.fill_rect 450 390 num_health 10)
+  if num_health > Character.get_hp enemy then
+    (* (Graphics.set_color (rgb 0 0 0); Graphics.fill_rect 450 390 100
+       10) *)
+    Raylib.draw_rectangle 450 390 100 10 Color.black
+  else
+    (* (Graphics.set_color (rgb 0 0 0); Graphics.fill_rect 450 390
+       num_health 10) *)
+    Raylib.draw_rectangle 450 390 num_health 10 Color.black
 
 let rec print_list (list : string list) =
   match list with
@@ -110,89 +80,74 @@ let rec print_list (list : string list) =
   | [] -> "none"
 
 let draw_battle_text bat () =
-  Graphics.set_color (rgb 0 0 0);
-  bottom_bar ();
+  (* Graphics.set_color (rgb 0 0 0); *)
+  (* bottom_bar (); *)
   health_bar_ally bat ();
   health_bar_enemy bat ();
-  Graphics.moveto 50 100;
-  Graphics.draw_string
-    ("Ally " ^ (bat |> Game.Battle.character |> Game.Character.get_id));
-  Graphics.moveto 50 370;
-  Graphics.draw_string
+  (* draw_text text pos_x pos_y font_size color *)
+  Raylib.draw_text
+    ("Ally " ^ (bat |> Game.Battle.character |> Game.Character.get_id))
+    50 100 20 Color.black;
+  Raylib.draw_text
     ("Team: "
     ^ (bat |> Game.Battle.team
       |> List.map (fun x -> Game.Character.get_id x)
-      |> print_list));
-  Graphics.moveto 400 140;
-  Graphics.moveto 315 60;
-  Graphics.draw_string
+      |> print_list))
+    50 370 20 Color.black;
+  Raylib.draw_text
     ("Press 1 for "
-    ^ Game.Character.get_action (Game.Battle.character bat) 0);
-  Graphics.moveto 315 40;
-  Graphics.draw_string
+    ^ Game.Character.get_action (Game.Battle.character bat) 0)
+    315 60 20 Color.black;
+  Raylib.draw_text
     ("Press 2 for "
-    ^ Game.Character.get_action (Game.Battle.character bat) 1);
-  Graphics.moveto 315 20;
-  Graphics.draw_string
+    ^ Game.Character.get_action (Game.Battle.character bat) 1)
+    315 40 20 Color.black;
+  Raylib.draw_text
     ("Press 3 for "
-    ^ Game.Character.get_action (Game.Battle.character bat) 2);
-  Graphics.moveto 485 55;
-  Graphics.draw_string "Press r to run!";
-  Graphics.moveto 485 25;
-  Graphics.draw_string "Press q to quit";
-  Graphics.moveto 450 370;
-  Graphics.draw_string
-    ("Enemy " ^ (bat |> Game.Battle.enemy |> Game.Character.get_id));
-  Graphics.set_color (rgb 255 0 0);
-  Graphics.moveto 50 90;
-  Graphics.draw_string
-    ("HP: " ^ string_of_int (Game.Battle.character_hp bat));
-  Graphics.moveto 450 350;
-  Graphics.draw_string
+    ^ Game.Character.get_action (Game.Battle.character bat) 2)
+    315 20 20 Color.black;
+  Raylib.draw_text "Press r to run!" 485 55 20 Color.black;
+  Raylib.draw_text "Press q to quit" 485 25 20 Color.black;
+  Raylib.draw_text
+    ("Enemy " ^ (bat |> Game.Battle.enemy |> Game.Character.get_id))
+    450 370 20 Color.red;
+  Raylib.draw_text
+    ("HP: " ^ string_of_int (Game.Battle.character_hp bat))
+    50 90 20 Color.black;
+  Raylib.draw_text
     ("HP: " ^ string_of_int (Game.Battle.enemy_hp bat))
+    450 350 20 Color.black
 
 (** not currently functional*)
 let draw_failed_run () =
-  Graphics.open_graph "";
-  Graphics.set_color (rgb 100 100 0);
-  Graphics.fill_rect 0 0 600 500;
-  Graphics.set_color (rgb 0 0 255);
-  Graphics.set_text_size 10000000;
-  Graphics.moveto 250 200;
-  Graphics.draw_string "You failed to run away"
+  Raylib.draw_rectangle 0 0 600 500 (Color.create 100 100 0 0);
+  Raylib.draw_text "You failed to run away" 250 200 10000
+    (Color.create 0 0 255 0)
 
 let victory_text () =
-  Graphics.open_graph "";
-  set_window_title "Victory";
-  Graphics.set_color (rgb 0 255 0);
-  Graphics.fill_rect 0 0 600 500;
-  Graphics.set_color (rgb 0 0 255);
-  Graphics.set_text_size 10000000;
-  Graphics.moveto 250 200;
-  Graphics.draw_string "Poggers!"
+  Raylib.set_window_title "Victory";
+  Raylib.draw_text "Poggers!" 250 200 10000 Color.green
+(* Graphics.set_color (rgb 0 255 0); Graphics.fill_rect 0 0 600 500;
+   Graphics.set_color (rgb 0 0 255); Graphics.set_text_size 10000000;
+   Graphics.moveto 250 200; Graphics.draw_string "Poggers!" *)
 
 let lose_text () =
-  Graphics.open_graph "";
+  (* Graphics.open_graph ""; *)
   set_window_title "Game Over";
-  Graphics.set_color (rgb 255 0 0);
-  Graphics.fill_rect 0 0 600 500;
-  Graphics.set_color (rgb 0 0 255);
-  Graphics.set_text_size 10000000;
-  Graphics.moveto 250 200;
-  Graphics.draw_string "Sadge D:"
+  Raylib.draw_rectangle 0 0 600 500 Color.black;
+  Raylib.draw_text "Sadge D:" 250 200 10000 Color.red
 
 let exit_battle bat =
   if Game.Battle.wonbattle bat then victory_text ()
   else if Game.Battle.losebattle bat then lose_text ()
 
 let rec wait (bat : Battle.t) =
-  Graphics.clear_graph ();
   exit_battle bat;
-  flush_kp ();
+  (* flush_kp (); *)
   draw_battle_text bat ();
   let character = Game.Battle.character bat in
   let enemy = Game.Battle.enemy bat in
-  let player_input = Graphics.read_key in
+  let player_input = Raylib.get_char_pressed in
   match Game.Command.battle_input bat character (player_input ()) with
   | Attack x ->
       bat
@@ -235,7 +190,6 @@ let randomChar3 =
   charArray |> Array.length |> Random.int |> Array.get charArray
 
 let battle_start () =
-  Graphics.open_graph " 1500 x 1500";
   set_window_title "Battle";
   let bat =
     Game.Battle.init_battle
@@ -259,18 +213,16 @@ let battle_start () =
   wait bat
 
 let rec map_wait st lvl =
-  Graphics.clear_graph ();
-  flush_kp ();
+  (* Graphics.clear_graph (); flush_kp (); *)
   Game.Level.draw_lvl lvl;
   let location = Game.State.current_tile_id st in
-  Graphics.set_color red;
-  Graphics.fill_circle (fst location) (snd location) 5;
-  let player_input = Graphics.read_key in
+  Raylib.draw_circle (fst location) (snd location) 5. Color.red;
+  let player_input = Raylib.get_char_pressed in
   match Game.Command.map_input (player_input ()) with
   | Up -> (
       let x = fst location in
       let y = snd location + 10 in
-      match Game.Level.get_tile (x / 10) (y / 10) lvl with
+      match Game.Level.get_tile (x / 96) (y / 96) lvl with
       | Grass -> map_wait (Game.State.move st x y) lvl
       | Water ->
           map_wait
@@ -310,25 +262,22 @@ let rec map_wait st lvl =
   | Exit -> exit 0
   | Invalid_input -> battle_start
 
-let setup () =
-  Raylib.init_window 1720 1000 "raylib [core] example - basic window";
-  Raylib.set_target_fps 60
-
-let main () =
-  setup ();
-  let lvl = Game.Level.init_lvl 100 100 in
-  Game.Level.draw_lvl lvl;
-  let c =
-    "data" ^ Filename.dir_sep ^ "char" ^ Filename.dir_sep ^ randomChar1
-    |> Yojson.Basic.from_file |> Game.Character.from_json
-  in
-  map_wait (Game.State.init_state lvl c) lvl
+(* let main () = let lvl = Game.Level.init_lvl 100 100 in
+   Game.Level.draw_lvl lvl; let c = "data" ^ Filename.dir_sep ^ "char" ^
+   Filename.dir_sep ^ randomChar1 |> Yojson.Basic.from_file |>
+   Game.Character.from_json in map_wait (Game.State.init_state lvl c)
+   lvl *)
 
 (* let main () = Graphics.open_graph " 1500 x 1500"; set_window_title
    "Title"; let lvl = Game.Level.init_lvl 100 100 in Game.Level.draw_lvl
    lvl; let c = "data" ^ Filename.dir_sep ^ "char" ^ Filename.dir_sep ^
    randomChar1 |> Yojson.Basic.from_file |> Game.Character.from_json in
    map_wait (Game.State.init_state lvl c) lvl *)
+let setup () =
+  Raylib.init_window 1720 1000 "raylib [core] example - basic window";
+  let lvl = Game.Level.init_lvl 100 100 in
+  Game.Level.draw_lvl lvl;
+  Raylib.set_target_fps 60
 
 let rec loop () =
   match Raylib.window_should_close () with
@@ -336,12 +285,11 @@ let rec loop () =
   | false ->
       let open Raylib in
       begin_drawing ();
-      clear_background Color.raywhite;
-      draw_text "Congrats! You created your first window!" 190 200 20
-        Color.lightgray;
+      (* clear_background Color.raywhite; *)
       end_drawing ();
       loop ()
 
-let () = main () () |> loop
+let () = setup () |> loop
+
 (* let () = setup () |> loop *)
 (* let () = interactive () let () = flush_kp () *)
