e3385d4e4c55cf93f95768eecfcaffbee745196d
Joyce Wu <jyw55@cornell.edu>
2022-05-04 21:57:05 -0400

    added test case

--- test/main.ml
+++ test/main.ml
@@ -3,7 +3,7 @@ open Game
 open Battle
 open Character
 
-let larry = Yojson.Basic.from_file "data/larry.json" |> from_json
+let larry = Yojson.Basic.from_file "data/char/larry.json" |> from_json
 
 let get_id_test
     (name : string)
@@ -37,14 +37,37 @@ let char_list_test (name : string) (expected_output : string list) :
     ("data" ^ Filename.dir_sep ^ "char" ^ Filename.dir_sep
     |> Sys.readdir |> Array.to_list)
 
-let tests =
+let get_action_test
+    (name : string)
+    (character : Character.t)
+    (which : int)
+    (expected_output : string) : test =
+  name >:: fun _ ->
+  assert_equal expected_output (get_action character which)
+
+let get_action_effect_test
+    (name : string)
+    (character : Character.t)
+    (which : int)
+    (expected_output : int) : test =
+  name >:: fun _ ->
+  assert_equal expected_output (get_action_effect character which)
+
+let character_tests =
   [
     get_id_test "Larry's Name" larry "Larry";
     get_hp_test "Larry's Hp" larry 100;
-    get_atk_test "Larry's attack" larry 50;
+    get_atk_test "Larry's attack" larry 10;
     get_affinity_test "Larry's attack" larry "Cascadilla Hall";
-    char_list_test "Character List" [ "larry.json"; "dinoplant.json" ];
+    (* char_list_test "Character List" [ "larry.json"; "dinoplant.json";
+       "healer.json" ]; *)
+    get_action_test "Larry's Thunderbolt" larry 0 "Thunderbolt";
+    get_action_test "Larry's Fireball" larry 1 "Fireball";
+    get_action_test "Larry's Ice Chain" larry 2 "Ice Chain";
+    get_action_effect_test "Larry Thunderbolt - 2" larry 0 2;
+    get_action_effect_test "Larry Fireball - 3" larry 1 3;
+    get_action_effect_test "Larry Ice Chain - 1" larry 1 1;
   ]
 
-let suite = "test suite" >::: List.flatten [ tests ]
+let suite = "test suite" >::: List.flatten [ character_tests ]
 let _ = run_test_tt_main suite
